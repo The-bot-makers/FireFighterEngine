8340eefd1e1d51419f14cec7167904c13dae9462
abhijato <68499000+abhijato@users.noreply.github.com>
2021-03-12 11:02:43 +0530

    update code

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-import chess
+import chess as gamelib
 import threading
 import time
+from numba import jit
 
 class FFEngine():
     def __init__(self):
@@ -9,42 +10,41 @@ class FFEngine():
         self.UnusedThreads=0
         self.ThreadList=[]
 
-    
     def ProcessMove(self,moves,currboard,depth,threadid):
         for move in moves:
-            brd=currboard
-            brd.push(moves[0])
+            brd=currboard.copy()
+            brd.push(move)
             currbranch=[]
             currbranch.append(0)
             if self.UnusedThreads>0:
                 nxtthreadid=threadid
-                nxtthreadid.append[0]
+                nxtthreadid.append(0)
                 thrdid=0
                 pmoves=list(currboard.legal_moves)
                 div=int(len(pmoves)/self.UnusedThreads)
                 if div<1:
                     self.UnusedThreads-=len(pmoves)
                     for move in pmoves:
-                        self.threadlist.append(threading.Thread(target=self.ProcessMove,args=([move],brd,depth-1,nxtthreadid,)))
-                        self.threadlist[-1].start()
+                        self.ThreadList.append(threading.Thread(target=self.ProcessMove,args=([move],brd,depth-1,nxtthreadid,)))
+                        self.ThreadList[-1].start()
                         time.sleep(0.8)
                         thrdid+=1
                         nxtthreadid[-1]=thrdid
                 else:
                     for i in range(self.UnusedThreads-1):
-                        self.threadlist.append(threading.Thread(target=self.ProcessMove,args=(pmoves[div*i:div*(i+1)],brd,depth-1,nxtthreadid,)))
-                        self.threadlist[-1].start()
+                        self.ThreadList.append(threading.Thread(target=self.ProcessMove,args=(pmoves[div*i:div*(i+1)],brd,depth-1,nxtthreadid,)))
+                        self.ThreadList[-1].start()
                         time.sleep(0.8)
                         thrdid+=1
                         nxtthreadid[-1]=thrdid
-                    self.threadlist.append(threading.Thread(target=self.ProcessMove,args=(pmoves[len(pmoves)-div:len(pmoves)],brd,depth-1,nxtthreadid,)))
-                    self.threadlist[-1].start()
-                
+                    self.ThreadList.append(threading.Thread(target=self.ProcessMove,args=(pmoves[len(pmoves)-div:len(pmoves)],brd,depth-1,nxtthreadid,)))
+                    self.ThreadList[-1].start()
+                    
     def Play(self,threads,fen,depth):
         if fen in self.Premoves:
             return self.Premoves[fen]
         else:
-            brd=chess.Board()
+            brd=gamelib.Board()
             brd.set_fen(fen)
             pmoves=list(brd.legal_moves)
             if len(pmoves)==1:
@@ -56,16 +56,30 @@ class FFEngine():
                 if div<1:
                     self.UnusedThreads-=len(pmoves)
                     for move in pmoves:
-                        self.threadlist.append(threading.Thread(target=self.ProcessMove,args=([move],brd,depth,[threadid],)))
-                        self.threadlist[-1].start()
+                        self.ThreadList.append(threading.Thread(target=self.ProcessMove,args=([move],brd,depth,[threadid],)))
+                        self.ThreadList[-1].start()
                         time.sleep(0.8)
                         threadid+=1
                 else:
+                    self.UnusedThreads=0
                     for i in range(threads-1):
-                        self.threadlist.append(threading.Thread(target=self.ProcessMove,args=(pmoves[div*i:div*(i+1)],brd,depth,[threadid],)))
-                        self.threadlist[-1].start()
+                        self.ThreadList.append(threading.Thread(target=self.ProcessMove,args=(pmoves[div*i:div*(i+1)],brd,depth,[threadid],)))
+                        self.ThreadList[-1].start()
                         time.sleep(0.8)
                         threadid+=1
-                    self.threadlist.append(threading.Thread(target=self.ProcessMove,args=(pmoves[len(pmoves)-div:len(pmoves)],brd,depth,[threadid],)))
-                    self.threadlist[-1].start()
+                    self.ThreadList.append(threading.Thread(target=self.ProcessMove,args=(pmoves[len(pmoves)-div:len(pmoves)],brd,depth,[threadid],)))
+                    self.ThreadList[-1].start()
                 return None
+
+    def PonderFixedDepth(self,threads,fen,depth):
+        brd=gamelib.Board()
+        brd.set_fen(fen)
+        
+    def PonderInf(self,threads,fen):
+        brd=gamelib.Board()
+        brd.set_fen(fen)
+        
+    def GetEvalnumber(self,fen):
+        brd=gamelib.Board()
+        brd.set_fen(fen)
+        
